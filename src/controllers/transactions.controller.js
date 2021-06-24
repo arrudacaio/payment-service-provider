7357af9fd5a36dd3230e398cea550e1b14de1de6
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-06-24 16:38:21 -0300

    feat: adds controller for transactions routes

--- src/controllers/transactions.controller.js
+++ src/controllers/transactions.controller.js
@@ -1,6 +1,14 @@
 
 
-const processTransaction = (req, res) => {}
+const testConnection = (_, res)  => {
+    try {
+        // call a service
+        return res.send("running test")
+    } catch (error) {
+        return console.log(error)
+    }
+}
 
-
-export default processTransaction
+module.exports = {
+    testConnection
+}
