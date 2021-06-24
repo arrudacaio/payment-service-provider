23e4019f76464ab5de415c55bd010b49c350135f
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-06-24 16:36:39 -0300

    feat: adds path for transactions routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,3 +1,7 @@
-import express from 'express'
-
+const express =  require('express')
+const TransactionsRoutes = require('./api/transactions.routes.')
 const router = express.Router()
+
+router.use('/transactions', TransactionsRoutes)
+
+module.exports = router
