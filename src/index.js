b6223b1ddf46d774b5dabb323bd788095fa3f731
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-06-24 16:37:40 -0300

    feat: adds path for version 1 of api

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import express from 'express'
-import cors from 'cors'
-// import router from ''
+const express = require('express')
+const cors = require('cors')
+const router = require('./routes/router')
+const PORT = process.env.PORT || 3000
 
 
-const app = express() // Creates a express application
-const PORT = 3000
+
+const app = express() 
 
 
 app.use(express.json()) 
 app.use(cors()) 
-// app.use(router)
+app.use('/v1', router)
 
 app.listen(PORT, () => {
     console.log(`App listening on port: ${PORT}`)
 })
+
