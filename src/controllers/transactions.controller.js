2bf8da89292988a518e5a1e16af482f9453cee39
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-06-25 23:22:03 -0300

    feat: adds method for delete transaction in controller

--- src/controllers/transactions.controller.js
+++ src/controllers/transactions.controller.js
@@ -13,13 +13,22 @@ const testConnection = (_, res)  => {
 // TODO  create a service for process data
 const createsTransaction = (_, res)  => {
     try {
-
-
         return res.send("running test")
     } catch (error) {
         return console.log(error)
     }
 }
+
+const deleteTransactions = (req, res) => {
+    try {
+        return console.log("Deleted transaction")
+    } catch (error) {
+        return console.log(error)
+    }
+
+}
+
+
 
 module.exports = {
     testConnection
