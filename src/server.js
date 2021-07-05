fa978c25da057dd0dbf81c3e0b462768931b6e9d
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-07-05 14:28:58 -0300

    fix: improve files

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(express.json()) 
 app.use(cors()) 
-app.use('/v1', router)
+app.use(router)
 
 app.listen(PORT, () => {
     console.log(`App listening on port: ${PORT}`)
